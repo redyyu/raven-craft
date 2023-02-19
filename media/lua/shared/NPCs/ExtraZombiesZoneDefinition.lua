2240e7cc99ab358e02330047f6fff8f2489d3385
Redy <redy.ru@gmail.com>
2023-02-20 05:14:09 +0800

    add to zombies

---
+++ media/lua/shared/NPCs/ExtraZombiesZoneDefinition.lua
@@ -0,0 +1,3 @@
+table.insert(ZombiesZoneDefinition.Default, {name = "PoliceRiotArmored", chance=2});
+table.insert(ZombiesZoneDefinition.Default, {name = "PoliceRiotArmored", chance=0.5, gender="female"});
+table.insert(ZombiesZoneDefinition.Police, {name = "PoliceRiotArmored", chance=20, gender="female"});
