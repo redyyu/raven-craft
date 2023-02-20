e7e89f607fad1a49033542c6f724ee17bc0735e1
Redy <redy.ru@gmail.com>
2023-02-20 15:44:22 +0800

    it's working
    `/` problem cause unable load vanilla models and texture

--- media/lua/shared/NPCs/ExtraZombiesZoneDefinition.lua
+++
@@ -1,3 +0,0 @@
-table.insert(ZombiesZoneDefinition.Default, {name = "PoliceRiotArmored", chance=2});
-table.insert(ZombiesZoneDefinition.Default, {name = "PoliceRiotArmored", chance=0.5, gender="female"});
-table.insert(ZombiesZoneDefinition.Police, {name = "PoliceRiotArmored", chance=20, gender="female"});
