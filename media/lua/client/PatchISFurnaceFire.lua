17b529888bfd916d5f8a755aeed5f8dd166b319d
Redy <redy.ru@gmail.com>
2023-02-22 00:02:22 +0800

    fixed furnace light working.

--- media/lua/client/PatchISFurnaceFire.lua
+++ media/lua/client/PatchISFurnaceFire.lua
@@ -2,45 +2,62 @@
 --**                    ROBERT JOHNSON                     **
 --***********************************************************
 
-require "TimedActions/ISBaseTimedAction/ISStopFurnaceFire"
-require "TimedActions/ISBaseTimedAction/ISFurnaceLightFromKindle"
-require "TimedActions/ISBaseTimedAction/ISFurnaceLightFromLiterature"
-require "TimedActions/ISBaseTimedAction/ISFurnaceLightFromPetrol"
+require "Blacksmith/TimedActions/ISStopFurnaceFire"
+require "Blacksmith/TimedActions/ISFurnaceLightFromKindle"
+require "Blacksmith/TimedActions/ISFurnaceLightFromLiterature"
+require "Blacksmith/TimedActions/ISFurnaceLightFromPetrol"
+require "TimedActions/ISBaseTimedAction"
 
-local SPRITES = {
-    fire_lit = "crafted_01_27",
-    fire_unlit = "crafted_01_26",
-    
-}
+local lit_sprite = "crafted_01_43";
+local unlit_sprite = "crafted_01_42"
 
-local ISStopFurnaceFireStop = ISStopFurnaceFire:stop;
-function ISStopFurnaceFire:stop()
-	ISStopFurnaceFireStop();
-    local isoObject = self:getIsoObject();
-	isoObject:setSprite(SPRITES.fire_unlit);
-    isoObject:transmitUpdatedSpriteToClients();
+function ISStopFurnaceFire:perform()
+    if self.furnace then
+        self.furnace:setSprite(unlit_sprite)
+    end
+    ISBaseTimedAction.perform(self);
+    self.furnace:setFireStarted(false);
 end
 
-local ISFurnaceLightFromKindleStart = ISFurnaceLightFromKindle:start
-function ISFurnaceLightFromKindle:start()
-	ISFurnaceLightFromKindleStart();
-    local isoObject = self:getIsoObject();
-	isoObject:setSprite(SPRITES.fire_lit);
-    isoObject:transmitUpdatedSpriteToClients();
+
+function ISFurnaceLightFromKindle:perform()
+	if self.furnace then
+        self.furnace:setSprite(lit_sprite)
+    end
+    if self.item then
+		self.item:getContainer():setDrawDirty(true);
+		self.item:setJobDelta(0.0);
+	end
+	self.plank:setJobDelta(0.0);
+	ISBaseTimedAction.perform(self);
 end
 
-local ISFurnaceLightFromLiteratureStart = ISFurnaceLightFromLiterature:start
-function ISFurnaceLightFromLiterature:start()
-	ISFurnaceLightFromLiteratureStart();
-    local isoObject = self:getIsoObject();
-	isoObject:setSprite(SPRITES.fire_lit);
-    isoObject:transmitUpdatedSpriteToClients();
+
+function ISFurnaceLightFromLiterature:perform()
+    if self.furnace then
+        self.furnace:setSprite(lit_sprite)
+    end
+    self.item:getContainer():setDrawDirty(true);
+    self.item:setJobDelta(0.0);
+	self.character:getInventory():Remove(self.item);
+	self.lighter:Use();
+	local fuelAmt = self.fuelAmt
+	if isClient() then
+		local cf = self.furnace
+		local args = { x = cf.x, y = cf.y, z = cf.z, fuelAmt = fuelAmt }
+	else
+        self.furnace:setFireStarted(true);
+	end
+	ISBaseTimedAction.perform(self);
 end
 
-local ISFurnaceLightFromPetrolStart = ISFurnaceLightFromPetrol:start
-function ISFurnaceLightFromPetrol:start()
-	ISFurnaceLightFromPetrolStart();
-    local isoObject = self:getIsoObject();
-	isoObject:setSprite(SPRITES.fire_lit);
-    isoObject:transmitUpdatedSpriteToClients();
+
+function ISFurnaceLightFromPetrol:perform()
+    if self.furnace then
+        self.furnace:setSprite(lit_sprite)
+    end
+    self.petrol:getContainer():setDrawDirty(true);
+    self.petrol:setJobDelta(0.0);
+    self.furnace:setFireStarted(true);
+	ISBaseTimedAction.perform(self);
 end
