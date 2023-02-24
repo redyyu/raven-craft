8cb9fb2652f11b69465512ead88277fe07d3ad61
Redy <redy.ru@gmail.com>
2023-02-25 03:06:16 +0800

    rain to water

--- media/lua/client/ExpanedISWorkbenchMenu.lua
+++ media/lua/client/ExpanedISWorkbenchMenu.lua
@@ -268,56 +268,7 @@ ISBuildBenchMenu.doBuildMenu = function(player, context, worldobjects, test)
         buildBenchMenu(workbenchMenu, workbenchOption, player);
     end
 
-
-    -- Path for Vanilla --
-    local furnace = nil;
-    local metal_drum = nil;
-
-    for i, v in ipairs(worldobjects) do
-        -- find Stone Furnace --
-        if instanceof(v, "BSFurnace") then
-            furnace = v;
-        end
-        -- find Metal Drum --
-        if CMetalDrumSystem.instance:isValidIsoObject(v) then
-            metal_drum = CMetalDrumSystem.instance:getLuaObjectOnSquare(v:getSquare())
-        end
-    end
-    
-    -- fix `Put out fire` on furance not change texture. --
-    if furnace and furnace:isFireStarted() then
-        context:removeOptionByName(getText("ContextMenu_Put_out_fire"));
-        context:addOption(getText("ContextMenu_Put_out_fire"), worldobjects, ISBuildBenchMenu.onStopFire, furnace, playerObj);
-    end
-
-    -- fix 5 Logs is too heavy. change to 2 --
-    if metal_drum and not metal_drum.haveLogs and not metal_drum.haveCharcoal then
-        local drumMenuOption = context:getOptionFromName(getText("ContextMenu_Metal_Drum"));
-        local subDrumMenu = context:getSubMenu(drumMenuOption.subOption);
-
-        -- subDrumMenu:removeOptionByName(getText("ContextMenu_Add_Logs"));
-        local addLogOption = subDrumMenu:getOptionFromName(getText("ContextMenu_Add_Logs"));
-        if addLogOption then
-            local tooltip = addLogOption.toolTip;
-            tooltip.description = getText("Tooltip_CHARCOAL_LOGS", 2);
-            addLogOption.notAvailable = playerInv:getItemCount("Base.Log") < 2;
-        end
-    end
-
 end
-
-
-ISBuildBenchMenu.onStopFire = function(worldobjects, furnace, player)
-    if luautils.walkAdj(player, furnace:getSquare()) then
-        ISTimedActionQueue.add(ISStopFurnaceFire:new(furnace, player))
-    end
-end
-
--- ISBuildBenchMenu.onAddLogs = function(worldobjects, metalDrum, player)
---     if luautils.walkAdj(player, metalDrum:getSquare()) then
---         ISTimedActionQueue.add(ISAddLogsInDrum:new(player, metalDrum, true))
---     end
--- end
 
 
 Events.OnFillWorldObjectContextMenu.Add(ISBuildBenchMenu.doBuildMenu)
---
+++ media/lua/server/ExpanedSnowToWater.lua
@@ -0,0 +1,96 @@
+if isClient() then return end
+
+require "MetalDrum/SMetalDrumGlobalObject.lua"
+require "RainBarrel/SRainBarrelSystem.lua"
+
+
+local EXCHANGE_RATIO = 10;
+
+
+-- Gather Snow for Metal Drum
+
+function SMetalDrumGlobalObject:update()
+    local isSnowing = getClimateManager():isSnowing();
+
+    if RainManager.isRaining() or isSnowing then
+        self.waterMax = self.waterMax or ISMetalDrum.waterMax
+        if self.waterAmount < self.waterMax then
+            local square = self:getSquare()
+            if square then self.exterior = square:isOutside() end
+            if self.exterior then
+                if not self.haveLogs and not self.haveCharcoal then
+                    if isSnowing then 
+                        self.waterAmount = math.min(self.waterMax, self.waterAmount + (1 * ISMetalDrum.waterScale)/EXCHANGE_RATIO)
+                    else
+                       self.waterAmount = math.min(self.waterMax, self.waterAmount + 1 * ISMetalDrum.waterScale)
+                    end
+                    self.taintedWater = true
+                    local isoObject = self:getIsoObject()
+                    if isoObject then -- object might have been destroyed
+                        self:noise('added rain to barrel at '..self.x..","..self.y..","..self.z..' waterAmount='..self.waterAmount)
+                        isoObject:setTaintedWater(true)
+                        isoObject:setWaterAmount(self.waterAmount)
+                        isoObject:transmitModData()
+                    end
+                end
+            end
+        end
+    end
+
+    if self.haveLogs and self.isLit then
+        if not self.charcoalTick then
+            self.charcoalTick = 1
+        else
+            self.charcoalTick = self.charcoalTick + 1
+        end
+        self:noise('charcoal update ' .. self.charcoalTick)
+        if self.charcoalTick == 12 then
+            self.haveLogs = false
+            self.isLit = false
+            self.haveCharcoal = true
+            self.charcoalTick = nil
+            if self.LightSource then
+                -- FIXME: won't be synced in multiplayer
+                getCell():removeLamppost(self.LightSource)
+            end
+        end
+        self:changeSprite()
+        self:setModData()
+    end
+end
+
+
+-- Gather the Snow for RainBarrel
+
+function SRainBarrelSystem:checkSnow()
+	if not getClimateManager():isSnowing() then return end
+	for i=1,self:getLuaObjectCount() do
+		local luaObject = self:getLuaObjectByIndex(i)
+		if luaObject.waterAmount < luaObject.waterMax then
+			local square = luaObject:getSquare()
+			if square then
+				luaObject.exterior = square:isOutside()
+			end
+			if luaObject.exterior then
+				luaObject.waterAmount = math.min(luaObject.waterMax, luaObject.waterAmount + (1 * RainCollectorBarrel.waterScale)/EXCHANGE_RATIO) --snow water is half of what should be with rain
+				luaObject.taintedWater = true
+				local isoObject = luaObject:getIsoObject()
+				if isoObject then -- object might have been destroyed
+					self:noise('added Snow to barrel at '..luaObject.x..","..luaObject.y..","..luaObject.z..' waterAmount='..luaObject.waterAmount)
+					isoObject:setTaintedWater(true)
+					isoObject:setWaterAmount(luaObject.waterAmount)
+					isoObject:transmitModData()
+				end
+			end
+		end
+	end
+end
+
+-- every 10 minutes we check if it's raining, to fill our water barrel
+local function EveryTenMinutesForSnow()
+	SRainBarrelSystem.instance:checkSnow()
+	-- SRainBarrelSystem.instance:checkRain()  comment it out otherwise might cast twice.
+end
+
+-- every 10 minutes we check if it's raining, to fill our water barrel
+Events.EveryTenMinutes.Add(EveryTenMinutesForSnow)
