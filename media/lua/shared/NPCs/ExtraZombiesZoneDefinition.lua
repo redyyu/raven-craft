f27cb14c2e842b982523a9500750c17da2e2122b
Redy <redy.ru@gmail.com>
2023-03-06 05:54:40 +0800

    minor fix

--- media/lua/shared/NPCs/ExtraZombiesZoneDefinition.lua
+++ media/lua/shared/NPCs/ExtraZombiesZoneDefinition.lua
@@ -1,6 +1,6 @@
-table.insert(ZombiesZoneDefinition.Default, {name = "ArmoredPolice", chance=1});
-table.insert(ZombiesZoneDefinition.Default, {name = "ArmoredArmy", chance=0.5});
-table.insert(ZombiesZoneDefinition.Default, {name = "ArmoredSurvivalist", chance=1});
+table.insert(ZombiesZoneDefinition.Default, {name = "ArmoredPolice", chance=0.2});
+table.insert(ZombiesZoneDefinition.Default, {name = "ArmoredArmy", chance=0.2});
+table.insert(ZombiesZoneDefinition.Default, {name = "ArmoredSurvivalist", chance=0.1});
 
 table.insert(ZombiesZoneDefinition.Police, {name = "ArmoredPolice", chance=6});
 table.insert(ZombiesZoneDefinition.PoliceState, {name = "ArmoredPolice", chance=2});
@@ -9,4 +9,4 @@ table.insert(ZombiesZoneDefinition.Prison, {name = "ArmoredPolice", chance=6});
 table.insert(ZombiesZoneDefinition.Army, {name = "ArmoredArmy", chance=6});
 table.insert(ZombiesZoneDefinition.SecretBase, {name = "ArmoredArmy", chance=6});
 
-table.insert(ZombiesZoneDefinition.Survivalist, {name = "ArmoredSurvivalist", chance=12});
+table.insert(ZombiesZoneDefinition.Survivalist, {name = "ArmoredSurvivalist", chance=6});
