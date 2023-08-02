9b7e131bcca7f0c4442172f87270ec5aab7e9f6a
Redy <redy.ru@gmail.com>
2023-08-03 04:01:33 +0800

    fix Sack Wheat distribution

--- media/lua/server/Items/Farming_Distribution.lua
+++ media/lua/server/Items/Farming_Distribution.lua
@@ -16,3 +16,15 @@ utils.insertDistribution(ProceduralDistributions.list["ToolStoreFarming"], ITEMS
 
 utils.insertDistribution(VehicleDistributions["FarmerGloveBox"], ITEMS_WEIGHT, 8);
 utils.insertDistribution(VehicleDistributions["FarmerTruckBed"], ITEMS_WEIGHT, 8);
+
+
+-- SackProduce
+
+utils.insertTable(ProceduralDistributions.list["GroceryStorageCrate1"], ".SackProduce_Wheat", 0.1)
+utils.insertTable(ProceduralDistributions.list["GroceryStorageCrate2"], ".SackProduce_Wheat", 4)
+utils.insertTable(ProceduralDistributions.list["GroceryStorageCrate3"], ".SackProduce_Wheat", 10)
+utils.insertTable(ProceduralDistributions.list["ProduceStorageCorn"], ".SackProduce_Wheat", 5)
+utils.insertTable(ProceduralDistributions.list["ProduceStorageLettuce"], ".SackProduce_Wheat", 5)
+utils.insertTable(ProceduralDistributions.list["ProduceStorageOnions"], ".SackProduce_Wheat", 5)
+utils.insertTable(ProceduralDistributions.list["ProduceStoragePotatoes"], ".SackProduce_Wheat", 5)
+utils.insertTable(ProceduralDistributions.list["CrateFarming"], ".SackProduce_Wheat", 2)
