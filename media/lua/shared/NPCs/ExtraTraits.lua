42a62e661ded0892eaad5d15f8730b90eb0f29d1
redyyu <redy.ru@gmail.com>
2024-02-24 16:47:43 +0800

    modify Traits, Handy add smallblunt +1

---
+++ media/lua/shared/NPCs/ExtraTraits.lua
@@ -0,0 +1,13 @@
+require "MainCreationMethods"
+
+local oldDoTraits = BaseGameCharacterDetails.DoTraits
+
+function ExpanedDoTraits()
+    oldDoTraits()
+    local handy = TraitFactory.getTrait("Handy")
+    handy:addXPBoost(Perks.SmallBlunt, 1)
+    handy:setDescription('') -- clear old description.
+    BaseGameCharacterDetails.SetTraitDescription(handy)
+end
+
+Events.OnGameBoot.Add(ExpanedDoTraits);
