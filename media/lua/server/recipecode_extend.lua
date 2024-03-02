16922f3d9f6c37cad9c482772891c2fad4c9a375
redyyu <redy.ru@gmail.com>
2024-03-02 09:46:24 +0800

    xxx

--- media/lua/server/recipecode_extend.lua
+++ media/lua/server/recipecode_extend.lua
@@ -470,3 +470,77 @@ end
 --         end
 --     end
 -- end
+
+
+-- require "TimedActions/ISAttachItemHotbar"
+
+
+function Recipe.OnCreate.RestoreBagItemsOnly(item, resultItem)
+
+	local transferred_Items = {}; 
+	local dItem;
+	
+	for i = 0, (item:size()-1) do 
+		dItem = item:get(i); 
+		if instanceof(dItem, "InventoryContainer") then 
+			dInv = dItem:getInventory(); 
+			newInv = resultItem:getInventory(); 
+			dInvItems = dInv:getItems(); 
+			if dInvItems:size() >= 1 then 
+				for i2 = 0, (dInvItems:size()-1) do
+					invItem = dInvItems:get(i2);
+					table.insert(transferred_Items, invItem) 
+				end
+			end
+		end
+	end
+	
+	for _, v in ipairs(transferred_Items) do
+		dInv:Remove(v); 
+		newInv:AddItem(v); 
+	end
+end
+
+
+function Recipe.OnCreate.RestoreBagItemsWithTexture(item, resultItem, player)
+	
+	local texture;
+
+	Recipe.OnCreate.RestoreBagItemsOnly(item, resultItem, player);
+
+	for i = 0, (item:size()-1) do 
+		dItem = item:get(i); 
+		if instanceof(dItem, "InventoryContainer") then 
+			texture = dItem:getTexture();
+		end
+	end
+
+	resultItem:setTexture(texture);
+end
+
+
+function Recipe.OnCreate.printArmyPackToBlack(item, resultItem, player)
+	
+	local texture;
+
+	Recipe.OnCreate.RestoreBagItemsOnly(item, resultItem, player);
+
+	resultItem:getVisual():setTextureChoice(1);
+end
+
+
+
+function Recipe.OnTest.IsEmptyBag(item)
+    if instanceof(item, "InventoryContainer") then
+        return item:getInventory():getItems():size() < 1;
+    end
+    return true
+end
+
+
+function Recipe.OnTest.IsEquippedBag(item)
+    if instanceof(item, "InventoryContainer") then
+        return not item:isEquipped();
+    end
+    return true
+end
