fd305823fd8dfcc448eabf36a207209015571c10
redyyu <redy.ru@gmail.com>
2024-03-16 19:26:54 +0800

    xxx

--- media/lua/server/recipecode_extend.lua
+++ media/lua/server/recipecode_extend.lua
@@ -103,17 +103,23 @@ function Recipe.OnCreate.CraftWeapon(items, result, player)
 end
 
 
-function Recipe.OnCreate.RemainCondition(items, result, player)
+function Recipe.OnCreate.RemainFirstItemCondition(items, result, player)
     if not result:getCondition() then return end
 
+    local fixs = 1  -- getHaveBeenRepaired starts with 1
     local condition = result:getCondition()
-    for i=0, items:size() - 1 do
-        local item = items:get(i)
-        if condition > item:getCondition() then
-            condition = item:getCondition()
+    if items:size() > 0 then
+        local item = items:get(0)
+        local src_condition = item:getCondition() or 0
+        if condition > src_condition then
+            condition = src_condition
+        end
+        if item:getHaveBeenRepaired() > fixs then
+            fixs = item:getHaveBeenRepaired()
         end
     end
     result:setCondition(condition)
+    result:setHaveBeenRepaired(fixs)
 end
 
 
