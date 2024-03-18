d94e09ea12b586893a2a7f09def36078ed70fc58
redyyu <redy.ru@gmail.com>
2024-03-19 00:28:24 +0800

    fix not smoke while too full

--- media/lua/client/Cigarettes/TimedActions/ISSmokeCigarettesPackAction.lua
+++ media/lua/client/Cigarettes/TimedActions/ISSmokeCigarettesPackAction.lua
@@ -35,11 +35,38 @@ function ISSmokeCigarettesPackAction:perform()
     self.cigarettes_pack:Use()
 	self.cigarettes_pack:setJobDelta(0.0)
 
+    ISBaseTimedAction.perform(self) 
+    -- `perform` must before the `eatItem`, because in `ISEatFoodAction` have `isValidStart()`
+    -- it will check MoodleLevel of Eaten, when character is too full, will stop the queue,
+    -- but `isValidStart()` only trigger when a queue action onComplete, it's not trigger by first action.
+    --[[ ** in `ISEatFoodAction` **
+        function ISEatFoodAction:isValidStart()
+	        return self.character:getMoodles():getMoodleLevel(MoodleType.FoodEaten) < 3 or self.character:getNutrition():getCalories() < 1000
+        end
+    ]]--
+    --[[ ** in `ISTimedActionQueue` **
+        function ISTimedActionQueue:onCompleted(action)
+            self:removeFromQueue(action)
+
+            self.current = self.queue[1]
+
+            if self.current then
+                if self.current:isValidStart() then
+                    self.current:begin()
+                else
+                    print('bugged action, cleared queue ', self.current.Type or "???")
+                    self:resetQueue()
+                    return
+                end
+            end
+        end
+    ]]--
+
     if RC.isRequireInHandOrInventory(self.character, self.cigarettes_pack) then
+        -- NO NEED this, `perform` before add new action to queue by `eatItem` is good enough.
+        -- ISTimedActionQueue:resetQueue()
         ISInventoryPaneContextMenu.eatItem(cigarettes, 1, self.character:getPlayerNum())
     end
-
-	ISBaseTimedAction.perform(self)
 end
 
 function ISSmokeCigarettesPackAction:new(character, cigarettes_pack)
