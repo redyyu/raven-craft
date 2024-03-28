a2a0b1a82801f87e52d9956e8f116de3b083e488
redyyu <redy.ru@gmail.com>
2024-03-29 02:42:54 +0800

    move boots to RCO

--- media/lua/server/Items/Clothes_Distribution.lua
+++ media/lua/server/Items/Clothes_Distribution.lua
@@ -3,23 +3,6 @@ require "Items/SuburbsDistributions"
 require "Vehicles/VehicleDistributions"
 
 
-local SHOES_WEIGHT = {
-    ["Shoes_BootsTINT"] = 1,
-    ["Shoes_FancyBoots"] = 1,
-}
-
-RC.insertDistTable(ProceduralDistributions.list["BandMerchShelves"], SHOES_WEIGHT, 6)
-RC.insertDistTable(ProceduralDistributions.list["BandPracticeClothing"], SHOES_WEIGHT, 6)
-RC.insertDistTable(ProceduralDistributions.list["ClosetShelfGeneric"], SHOES_WEIGHT, 0.5)
-RC.insertDistTable(ProceduralDistributions.list["ClothingStorageFootwear"], SHOES_WEIGHT, 6)
-RC.insertDistTable(ProceduralDistributions.list["ClothingStoresBoots"], SHOES_WEIGHT, 10)
-RC.insertDistTable(ProceduralDistributions.list["CrateFootwearRandom"], SHOES_WEIGHT, 10)
-RC.insertDistTable(ProceduralDistributions.list["CrateRandomJunk"], SHOES_WEIGHT, 0.2)
-RC.insertDistTable(ProceduralDistributions.list["FactoryLockers"], SHOES_WEIGHT, 4)
-
-RC.insertDistTable(VehicleDistributions["ClothingTruckBed"], SHOES_WEIGHT, 0.6)
-
-
 local CORSET_WEIGHT = {
     [".Corset_Pink"] = 2,
     [".Corset_BodysuitBlack"] = 2,
