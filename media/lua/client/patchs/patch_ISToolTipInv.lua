fd3540be3fa7ec1602304301d2485abbeb9939bc
redyyu <redy.ru@gmail.com>
2024-04-10 18:20:37 +0800

    remove unused

--- media/lua/client/patchs/patch_ISToolTipInv.lua
+++ media/lua/client/patchs/patch_ISToolTipInv.lua
@@ -9,17 +9,9 @@ local old_ISToolTipInv_render = ISToolTipInv.render
 
 function ISToolTipInv:render()
 	if not self.item then return false end
-	local item = self.item
-	local player = getPlayer()
 
-	if item and instanceof(item, "Clothing")
-	   and item:getBodyLocation()
-	   and player:getWornItem(item:getBodyLocation())
-	   and instanceof(player:getWornItem(item:getBodyLocation()), "InventoryContainer")
-	then
-		return false
-	elseif item:getFullType() == RC.getPackageItemType('.SurvivalJournal') then
-		SurvivalJournal.setTooltip(item)
+	if self.item:getFullType() == RC.getPackageItemType('.SurvivalJournal') then
+		SurvivalJournal.setTooltip(self.item)
 	end
 
 	old_ISToolTipInv_render(self)
