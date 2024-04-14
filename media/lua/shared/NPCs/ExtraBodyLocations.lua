2c47d1d1b1df860d7633e1300d7fba6b36dc537f
redyyu <redy.ru@gmail.com>
2024-04-14 20:54:48 +0800

    hide armor for some clothes

--- media/lua/shared/NPCs/ExtraBodyLocations.lua
+++ media/lua/shared/NPCs/ExtraBodyLocations.lua
@@ -24,6 +24,29 @@ group:setExclusive("ArmorFull", "ArmorNeck")
 
 group:getOrCreateLocation("TightMask")  -- for some mask wearing with FullHat, Masks or Glasses.
 
+group:setHideModel("BathRobe", "ArmorFull")
+group:setHideModel("BathRobe", "ArmorArms")
+group:setHideModel("BathRobe", "ArmorLegs")
+group:setHideModel("BathRobe", "ArmorShoulder")
+group:setHideModel("BathRobe", "ArmorHands")
+group:setHideModel("BathRobe", "ArmorNeck")
+
+group:setHideModel("FullSuitHead", "ArmorFull")
+group:setHideModel("FullSuitHead", "ArmorArms")
+group:setHideModel("FullSuitHead", "ArmorLegs")
+group:setHideModel("FullSuitHead", "ArmorShoulder")
+group:setHideModel("FullSuitHead", "ArmorHands")
+group:setHideModel("FullSuitHead", "ArmorNeck")
+
+group:setHideModel("FullSuit", "ArmorFull")
+group:setHideModel("FullSuit", "ArmorArms")
+group:setHideModel("FullSuit", "ArmorLegs")
+group:setHideModel("FullSuit", "ArmorShoulder")
+group:setHideModel("FullSuit", "ArmorHands")
+group:setHideModel("FullSuit", "ArmorNeck")
+
+
+
 -- VERY IMPORTANT TIPs.
 -- if got DoSomthing(BloodBodyPartType) Error.
 -- BloodBodyPartType is null.
